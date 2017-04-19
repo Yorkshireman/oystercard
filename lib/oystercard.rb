9a35ee0670dc3dca50cd41587760492d9a48c2a9
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2017-04-20 00:37:38 +0100

    max balance test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard # :nodoc:
+  MAXIMUM_ALLOWED_BALANCE = 90
   attr_accessor :balance
 
   def initialize
@@ -6,6 +7,16 @@ class Oystercard # :nodoc:
   end
 
   def top_up(amount)
+    if top_up_would_exceed_max_balance?(amount)
+      raise ArgumentError, 'maximum balance allowed is 90'
+    end
+
     self.balance += amount
+  end
+
+  private
+
+  def top_up_would_exceed_max_balance?(amount)
+    (amount + self.balance) > MAXIMUM_ALLOWED_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Oystercard do
   it 'can be topped-up by a stipulated amount' do
     expect { subject.top_up(10) }.to change { subject.balance }.by(10)
   end
+
+  it 'has a maximum balance of 90' do
+    expect { subject.top_up(91) }.to(
+      raise_error(ArgumentError, 'maximum balance allowed is 90')
+    )
+  end
 end
