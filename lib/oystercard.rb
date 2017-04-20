caaecc516771529b46609f15890ae45ea366f326
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2017-04-20 02:00:32 +0100

    touch-in minimum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
+MAXIMUM_ALLOWED_BALANCE = 90
+MINIMUM_TOUCH_IN_BALANCE = 1
+
 class Oystercard # :nodoc:
-  MAXIMUM_ALLOWED_BALANCE = 90
   attr_accessor :balance
+  attr_accessor :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -19,9 +23,27 @@ class Oystercard # :nodoc:
     self.balance -= fare
   end
 
+  def touch_in
+    if minimum_balance_not_met?
+      # rubocop:disable LineLength
+      raise "balance of #{self.balance} is less than minimum balance of #{MINIMUM_TOUCH_IN_BALANCE}"
+      # rubocop:enable LineLength
+    end
+
+    self.in_journey = true
+  end
+
+  def touch_out
+    self.in_journey = false
+  end
+
   private
 
   def top_up_would_exceed_max_balance?(amount)
     (amount + self.balance) > MAXIMUM_ALLOWED_BALANCE
+  end
+
+  def minimum_balance_not_met?
+    self.balance < MINIMUM_TOUCH_IN_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,24 +1,57 @@
 require 'spec_helper'
 require 'oystercard'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Oystercard do
-  it 'has an opening balance of zero' do
-    expect(subject.balance).to eq 0
+  describe 'defaults' do
+    it 'has an opening balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+
+    it 'has an opening \'in_journey\' status of \'false\'' do
+      expect(subject.in_journey).to eq false
+    end
   end
 
-  it 'can be topped-up by a stipulated amount' do
-    expect { subject.top_up(10) }.to change { subject.balance }.by(10)
-  end
+  context 'topping-up' do
+    it 'can be topped-up by a stipulated amount' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+    end
 
-  it 'has a maximum balance of 90' do
-    expected_message = 'top-up of 91 would exceed maximum allowed balance'
-    expect { subject.top_up(91) }.to(
-      raise_error(ArgumentError, expected_message)
-    )
+    it 'has a maximum balance' do
+      expected_message = 'top-up of 91 would exceed maximum allowed balance'
+      expect { subject.top_up(91) }.to(
+        raise_error(ArgumentError, expected_message)
+      )
+    end
   end
 
   it 'can have a fare deducted from its balance' do
-    subject.top_up(10)
+    subject.balance = 10
     expect { subject.deduct(5) }.to change { subject.balance }.by(-5)
   end
+
+  context 'touching-in/out' do
+    context 'when balance is not less than MINIMUM_TOUCH_IN_BALANCE' do
+      it 'can touch-in' do
+        subject.balance = MINIMUM_TOUCH_IN_BALANCE
+        expect { subject.touch_in }.to change { subject.in_journey }.to true
+      end
+    end
+
+    it 'can touch-out' do
+      subject.in_journey = true
+      expect { subject.touch_out }.to change { subject.in_journey }.to false
+    end
+
+    context 'when balance is less than MINIMUM_TOUCH_IN_BALANCE' do
+      it 'refuses to touch-in' do
+        expected_message = 'balance of 0 is less than minimum balance of 1'
+        expect { subject.touch_in }.to raise_error(
+          RuntimeError, expected_message
+        )
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
